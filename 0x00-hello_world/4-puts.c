ea3f6cbd6e0bfb923fee3e151a3d7432f2e91350
iynulwa <joshuaiyanuoluwa1@gmail.com>
2022-09-09 16:28:41 -0700

    Executable file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
- * *main -Entry point 
+ * *main -Entry point
  *
  * *Return: Always 0 (Success)
  *
@@ -10,4 +10,3 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
