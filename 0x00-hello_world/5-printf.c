526a02fc5056482ea630371a59f17da9095d6153
iynulwa <joshuaiyanuoluwa1@gmail.com>
2022-09-09 17:24:52 -0700

    Use printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+
 /**
  * *main - Entry point
  *
@@ -8,7 +9,9 @@
 int main(void)
 
 {
+
 	printf("with proper grammer, but the outcome is a piece of art,\n");
 
 		return (0);
+
 }
