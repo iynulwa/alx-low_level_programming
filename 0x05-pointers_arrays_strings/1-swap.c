dc9cadd8af4d0609594b11f4f738454d8eb4231f
iynulwa <joshuaiyanuoluwa1@gmail.com>
2022-09-19 14:25:59 -0700

    Swap values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - Interchanging pointer values
+ * @a: int type pointer
+ * @b: int type pointer
+ */
+
+void swap_int(int *a, int *b)
+{
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
