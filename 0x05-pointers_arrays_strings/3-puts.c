fcf24bec7273b4708f46e931dcf4c65766a29047
iynulwa <joshuaiyanuoluwa1@gmail.com>
2022-09-19 16:52:15 -0700

    Print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints string to standard output
+ * @str: chat type string
+ * Return: char array string type
+ */
+
+void _puts(char *str)
+{
+	int n;
+
+	for (n = 0; str[n] != '\0'; str++);
+	{
+		_putchar(str[n]);
+	}
+	_putchar('\n');
+}
