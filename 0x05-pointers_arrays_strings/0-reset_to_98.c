c366b1efb78d42a19ae36b6d4b66117cc29a8112
iynulwa <joshuaiyanuoluwa1@gmail.com>
2022-09-19 13:58:39 -0700

    Header and Readme

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,11 @@
 #include "main.h"
 
 /**
- * reset_to_98 - entry point
- *
- * Description: Changes the value of a variable using a pointer
- *
- * Return: Always 0
+ * reset_to_98 - Set pointer parameter to 98
+ * @n: int type pointer
  */
 
-void return_to_98(int *n)
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
