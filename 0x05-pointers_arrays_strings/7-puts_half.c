533b87c5ad0c7c6f0952ca0061e9d9892f9281cb
iynulwa <joshuaiyanuoluwa1@gmail.com>
2022-09-19 17:42:34 -0700

    Other half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints the second half of a string
+ * @str: char type string
+ * Return: Other half of string.
+ */
+
+void puts_half(char *str)
+{
+	int n;
+
+	for (n = 0; str[n] != '\0'; n++)
+		;
+
+	i++;
+	for (i =/ 2; str[n] != '\0'; n++)
+	{
+		_putchar(str[n]);
+	}
+	_putchar('\n');
+}
