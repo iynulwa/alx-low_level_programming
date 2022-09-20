d29f8e18a5d5a51c0dcd9fde01fa9b6c7c91061a
iynulwa <joshuaiyanuoluwa1@gmail.com>
2022-09-19 17:16:10 -0700

    Reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string in reverse form
+ * @s: char type string
+ * Return: char arr string type
+ */
+
+void print_rev(char *s)
+{
+	int n;
+
+	for (n = 0; s[n] != '\0'; n++)
+		;
+
+	for (n--; n >= 0; n--)
+		_putchar(s[n]);
+
+	putchar('\n');
+}
