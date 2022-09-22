de423d0353dd885d0474dcce65490a919f061e2e
iynulwa <joshuaiyanuoluwa1@gmail.com>
2022-09-22 09:44:57 -0700

    Print numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,50 +1,29 @@
 #include "main.h"
-#include <stdio.h>
+
+void print_vint(unsigned int n);
 
 /**
- * print_number - print an integer using _putchar
- * @n: integer to print
+ * print_number - print an integer
+ * @n: The integer to print
  */
-
 void print_number(int n)
 {
-	int power;
-	int neg;
-	int hold;
-
-	neg = 0;
-	power = 1;
-	hold = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		neg = 1;
+		n = -n;
 	}
 
-	while (hold > 9 || hold < -9)
-	{
-		power *= 10;
-		hold /= 10;
-	}
+	print_unit((unsigned int) n);
+}
 
-	while (power > 0)
-	{
-		if (power > 9)
-		{
-			if (!neg)
-				_putchar((n / power % 10) + '0');
-			else
-				_putchar((n / power % 10) * -1 + '0');
-
-			power /= 10;
-		}
-		if (power == 1)
-		{
-			if (neg)
-				_putchar((n % 10) * -1 + '0');
-			else
-				_putchar(n % 10 + '0');
-			power = 0;
-		}
-	}
+/**
+ * print_unit - prints an unsigned integer
+ * @n: The unsigned int to print
+ */
+void print_unit(unsigned int n)
+{
+	if (n / 10 != 0)
+		print_unit(n / 10);
+	_putchar(n % 10 + '0');
 }
