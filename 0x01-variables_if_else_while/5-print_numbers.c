0304a27d8511ba76d5c4b5f41745f66856b247f7
iynulwa <joshuaiyanuoluwa1@gmail.com>
2022-09-30 08:21:15 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main entry
+ * Description: Prints numbers in base 10
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+	c = 0;
+	while (c <= 10)
+	{
+		putchar (c);
+		c++;
+	}
+	putchar('\n');
+}
