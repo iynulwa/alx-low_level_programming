a04986e4d8ee06ca686adf3564db6546c6c77043
iynulwa <joshuaiyanuoluwa1@gmail.com>
2022-10-01 07:21:55 -0700

    Print Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: Print all numbers in base 16 in lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		putchar (c);
+	}
+
+	c = 'a'
+	for (c = 'a'; c <= f; c++)
+	{
+		putchar (c);
+	}
+	putchar ('\n');
+	return (0);
+}
