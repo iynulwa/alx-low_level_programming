7dfd36ffb8cb0fc12ed21204ca43187797137472
iynulwa <joshuaiyanuoluwa1@gmail.com>
2022-10-01 06:20:55 -0700

    In reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: print lowercase letters in reverse from z -a
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c++)
+	{
+		putchar (c);
+	}
+	putchar ('\n');
+	return (0);
+}
