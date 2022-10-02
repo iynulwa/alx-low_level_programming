6e4eb69e32bcde72cb24d853648e4f0f185ff784
iynulwa <joshuaiyanuoluwa1@gmail.com>
2022-10-02 09:25:31 -0700

    Multiply

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - multiplies two numbers
+ * @argc: argument number
+ * @argv: argument array
+ * Return: value of mul if successful
+ * print error and return 1 if unsuccessful
+ */
+int main(int argc, char *argv[])
+{
+	int n
