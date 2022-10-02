090c56baefeba7cf8e6b03b33d7b114c01a3f652
iynulwa <joshuaiyanuoluwa1@gmail.com>
2022-10-01 19:30:32 -0700

    Count

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,11 +6,10 @@
  * @argc: Argument count
  * @argv: Pointer to string
  *
- * Return 0
+ * Return 0 (Success)
  */
 int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
-
 	return (0);
 }
