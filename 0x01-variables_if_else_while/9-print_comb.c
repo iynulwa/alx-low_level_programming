6af7b6b14c7ce419081f298715bc6fc0cd442c5d
iynulwa <joshuaiyanuoluwa1@gmail.com>
2022-10-02 19:41:12 -0700

    Print combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *
+ * main -main block
+ * Descrription: Prints all possible combination of single digit numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar (i + '0');
+			if (i < 9)
+			{
+				putchar (', ');
+			}
+	}
+	putchar('\n');
+	return (0);
+}
