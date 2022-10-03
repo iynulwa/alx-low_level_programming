cef2473f0d77bca820bc5363cb5c9f7ec1ca2e6e
iynulwa <joshuaiyanuoluwa1@gmail.com>
2022-10-02 19:53:01 -0700

    Print combo

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,11 +14,15 @@ int main(void)
 	for (i = 0; i < 10; i++)
 	{
 		putchar (i + '0');
-			if (i < 9)
+		if (i < 9)
+		{
+			putchar (',');
+		}
+			if ( i < 9)
 			{
-				putchar (, );
+				putchar (' ');
 			}
+			putchar ('\n');
 	}
-	putchar('\n');
 	return (0);
 }
