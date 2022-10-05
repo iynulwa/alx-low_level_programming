5ad08a51ef1aee449cb2b2b836a53d8e31a97dd0
iynulwa <joshuaiyanuoluwa1@gmail.com>
2022-10-05 09:15:42 -0700

    Two combos

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,7 +3,8 @@
 /**
  * main - main block
  * Description: Print different combo of two digits
- * combos like 21 and 12 are considered the same
+ * combos like 21 and 12 are considered the same combo
+ * print smaller combos of two digits
  *
  * Return: 0
  */
@@ -12,25 +13,27 @@ int main(void)
 	int i, j, k;
 
 	i = 0;
+
 	while (i < 100)
 	{
 		j = i % 10;
 		k = i / 10;
-		
+
 		if (k < j)
 		{
-			putchar (j + '0');
-			putchar (k + '0');
+			putchar(k + '0');
+			putchar(j + '0');
 
 			if (i < 89)
 			{
-				putchar (',');
-				putchar (' ');
+				putchar(',');
+				putchar(' ');
 			}
 		}
 
 		i++;
 	}
-	putchar ('\n');
+	putchar('\n');
+
 	return (0);
 }
