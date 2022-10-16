9ebb6c3d95a8cdcba0c19633015587496d50d074
iynulwa <joshuaiyanuoluwa1@gmail.com>
2022-10-16 16:44:33 -0700

    Last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,15 +6,5 @@
  */
 int print_last_digit(int nld)
 {
-	int pld;
-
-	pld = (nld % 10);
-
-	if (pld < 0)
-	{
-		pld = (-1 * pld);
-	}
-
-	_putchar(pld + '0');
-	return (pld);
+	return (nld % 10);
 }
